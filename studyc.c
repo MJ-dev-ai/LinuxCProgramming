05132f60eb29c1f16997dc3b5541fd2e8bbb314d
MingyuJang <mingyu.jang.dev@gmail.com>
2025-10-28 12:58:55 +0900

    함수 포인터 예제 수정

--- studyc.c
+++ studyc.c
@@ -8,11 +8,33 @@ void swap(double* a, double* b);
 void line_up(double*, double*, double*);
 void print_array(int* pa, int size);
 void print_str(char** ary, int size);
+void func(int (*fp)(int, int));
+int sum(int a, int b) { return (a + b); }
+int max(int a, int b) { return (a > b) ? a : b; }
+int mul(int a, int b) { return (a * b); }
 
 int main(void) {
-
+    int select;
+    int (*fp[4])(int, int) = {NULL, sum, mul, max};
+    printf("1 두 정수의 합\n2 두 정수의 곱\n3 두 정수중 큰 값 계산\n");
+    printf("원하는 연산을 선택하세요: ");
+    scanf("%d", &select);
+    // switch (select) {
+    //     case 1: func(sum); break;
+    //     case 2: func(mul); break;
+    //     case 3: func(max); break;
+    // }
+    if (select > 0 && select < 4) func(fp[select]);
 
     return 0;
+}
+
+void func(int (*fp)(int, int)) {
+    int a, b;
+    printf("두 정수를 입력하세요: ");
+    scanf("%d%d", &a, &b);
+    int res = fp(a, b);
+    printf("결과값: %d\n", res);
 }
 
 void swap_ptr(char** ppa, char** ppb) {
